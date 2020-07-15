c2d920ef90e3693a230f5990b49263b615ecce6c
lvivduncan <lvivduncan@gmail.com>
2020-07-15 11:50:27 +0300

    class levusAccordion

--- js/levus-accordion.js
+++ js/levus-accordion.js
@@ -1,5 +1,50 @@
-document.querySelectorAll('.levus-accordion .title').forEach(item => {
+// задротський варіант =)
+// document.querySelectorAll('.levus-accordion .title').forEach(item => item.addEventListener('click', () => item.nextElementSibling.classList.toggle('show')));
+
+/* document.querySelectorAll('.levus-accordion .title').forEach(item => {
     item.addEventListener('click', function() {
-        item.nextElementSibling.classList.toggle('show');
-    });
-});
+        this.classList.toggle('open');
+        this.nextElementSibling.classList.toggle('show');
+    })
+}); */
+
+class Accordion {
+    constructor(title) {
+            this.title = document.querySelectorAll(title);
+        }
+        // якщо тре розкривати тільки 1
+    single() {
+        this.title.forEach(item => {
+            item.addEventListener('click', function() {
+                this.classList.toggle('open');
+                this.nextElementSibling.classList.toggle('show');
+            })
+        });
+    }
+    static closeAll() {
+            this.title.forEach(item => {
+                console.log(this.title)
+                console.log(item)
+            });
+        }
+        // якщо розкриває 1 і закриває усі решта
+    multiply() {
+        this.title.forEach(item => {
+            const all = this.title;
+
+            item.addEventListener('click', function() {
+
+                all.forEach(item => {
+                    item.classList.remove('open');
+                    item.nextElementSibling.classList.remove('show');
+                });
+
+                this.classList.toggle('open');
+                this.nextElementSibling.classList.toggle('show');
+            })
+        });
+    }
+}
+
+// document.querySelectorAll('.levus-accordion .title') && new Accordion('.levus-accordion .title').single();
+document.querySelectorAll('.levus-accordion .title') && new Accordion('.levus-accordion .title').multiply();
